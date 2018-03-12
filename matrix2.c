5156d768b6d8832691ee1b2f641927f667ee5fec
giwrgospan <giwrgospan277@gmail.com>
2018-03-13 00:23:16 +0200

    Update matrix2.c

--- matrix2.c
+++ matrix2.c
@@ -2,53 +2,59 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix2.c -o matrix2
+// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix1.c -o matrix1
 
 
 #define NCOLS 100
 
 void get_walltime(double *wct) {
-  struct timeval tp;
-  gettimeofday(&tp,NULL);
-  *wct = (double)(tp.tv_sec+tp.tv_usec/1000000.0);
+	struct timeval tp;
+	gettimeofday(&tp,NULL);
+	*wct = (double)(tp.tv_sec+tp.tv_usec/1000000.0);
 }
 
-
 int main() {
+
 double *table;
 double ts,te;
+double sum=0.;
 
 
-  table = (double *)malloc(NROWS*NCOLS*sizeof(double)); 
-  if (table==NULL) {
-    printf("alloc error!\n");
-    exit(1);
-  }
+	table = (double *)malloc(NROWS*NCOLS*sizeof(double));
+	if (table==NULL) {
+	printf("alloc error!\n");
+	exit(1);
+	}
 
-  // warmup
+	
+	for (int i = 0; i < NROWS * NCOLS; i++){
+		table[i] = 2.;
+	}
 
-  // ...your code here...
+	
+	get_walltime(&ts);
+	
+	for(int j = 0; j < NCOLS; j++) {
 
-  // get starting time (double, seconds) 
-  get_walltime(&ts);
+		for(int i = 0; i < NROWS; i++) {
+
+			sum += table[i * NCOLS + j];
+		}
+	}
+
+
+	get_walltime(&te);
+	
+	double time = te - ts;
+	
+	printf ( " Time took = %lf\n" , time ); 
+		
+	printf("sum= %f\n",sum);
+	
+	
   
-  // workload
 
-  // ...your code here...
-
-  // get ending time
-  get_walltime(&te);
-
-  // check results
-  
-  // ...your code here...
-
-  // print time elapsed and/or Maccesses/sec
-  
-  // ...your code here...  
-  
   free(table);
 
   return 0;
 }
-
